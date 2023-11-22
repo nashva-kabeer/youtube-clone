6f2179c1aa9607871260050ff3e67365db3a4660
Fathima Nashva <nashvakabeer17@gmail.com>
2023-11-22 13:13:11 +0530

    video grid set

---
+++ client/src/components/ShowVideo/ShowVideo.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./ShowVideo.css";
+
+function ShowVideo({vid}) {
+  return (
+    <>
+    <div>
+        <video 
+        src={`${vid?.video_src}`}
+        className='video_ShowVideo'
+        />
+    </div>
+    <div className="video_description">
+        <div className="Chanel_logo_App">
+            <div className="fstChar_logo_App">
+                <>{vid?.Uploader?.charAt(0).toUpperCase()}</>
+            </div>
+        </div>
+        <div className="video_details">
+            <p className="title_video_ShowVideo">title</p>
+            <pre className='Vid_Views_UploadTime'>1-1-2022</pre>
+            <pre className='Vid_Views_UploadTime'>5 views <div className="dot"></div> video uploaded 1 year ago</pre>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default ShowVideo
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
 import './Home.css';
 import LeftSidebar from '../../components/LeftSidebar/LeftSidebar';
+import ShowVideoGrid from '../../components/ShowVideoGrid/ShowVideoGrid';
+import vid from '../../components/Video/vid.mp4'
 
 function Home() {
+  const vids = [
+      {
+        _id: 1,
+        video_src: vid,
+        Chanel: "62bafe6752cea35a6c30685f",
+        title: "video 1",
+        Uploader: "abc",
+        description: "description of  video 1",
+      },
+      {
+        _id: 2,
+        video_src: vid,
+        Chanel: "cdd",
+        title: "video 2",
+        Uploader: "abc",
+        description: "description of  video 2",
+      },
+      {
+        _id: 3,
+        video_src: vid,
+        Chanel: "add",
+        title: "video 3",
+        Uploader: "abc",
+        description: "description of  video 3",
+      },
+      {
+        _id: 4,
+        video_src: vid,
+        Chanel: "add",
+        title: "video 3",
+        Uploader: "abc",
+        description: "description of  video 3",
+      },
+    ];
+
   return (
     <div className='container_Pages_App'>
         <LeftSidebar/>
         <div className="container2_Pages_App">
-
+          <ShowVideoGrid vids={vids}/>
         </div>
     </div>
   )
